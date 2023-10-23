c60aed34a69f38e27d41948c578e256918e3523d
regul951 <regul951@gmail.com>
2023-10-23 21:35:38 +0300

    HW_04

---
+++ Ex_003/Program.cs
@@ -0,0 +1,86 @@
+﻿// using System;
+
+// public class Answer
+// {
+//     static bool IsPalindrome(int number)
+//     {
+//         bool result = false;
+//         string numberLength = number + "";
+//         if (numberLength.Length > 5)
+//         {
+//             Console.WriteLine("Число не пятизначное");
+//             return result;
+//         }
+//         else
+//         {
+//             for (int i = 0; i < numberLength.Length / 2; i++)
+//             {
+//                 if (numberLength[i] == numberLength[numberLength.Length - 1 - i])
+//                 {
+//                     result = true;
+//                 }
+//                 else
+//                 {
+//                     result = false;
+//                     return result;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+
+//     // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args)
+//     {
+//         int number;
+
+//         if (args.Length >= 1)
+//         {
+//             number = int.Parse(args[0]);
+//         }
+//         else
+//         {
+//             // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             number = 64546;
+//         }
+
+//         // Не удаляйте строки ниже
+//         bool result = IsPalindrome(number);
+//         System.Console.WriteLine($"{result}");
+//     }
+// }
+
+
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+        if (number < 10000 || number >= 100000){
+            Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+
+        int reverse = 0;
+        int temp = number;
+        while (temp > 0){
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+            Console.WriteLine($"{reverse}, {temp}");
+        }
+        return reverse == number;
+    }
+
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+            number = 64546;
+        }
+
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
---
+++ Ex_004/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+static int exponentiation(int numA, int numB)
+{
+    int result = numA;
+    int i = 2;
+    while (i <= numB)
+    {
+        result *= numA;
+        i++;
+    }
+    return result;
+}
+
+int numA = 3;
+int numB = 5;
+
+int result25 = exponentiation(numA, numB);
+Console.WriteLine($"result25: {result25}\n");
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+static int sumNumber(int num)
+{
+    int sum = 0;
+    int numLength = (int)Math.Log10(num) + 1;
+    for (int i = 0; i <= numLength; i++)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int num = 33;
+
+int result27 = sumNumber(num);
+Console.WriteLine($"result27: {result27}\n");
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void prntArray(int[] ar)
+{
+    foreach (var num in ar)
+    {
+        
+        Console.WriteLine(num);
+    }
+}
+
+int[] arr = { 1, 2, 5, 7, 19, 6, 1, 33 };
+
+Console.WriteLine("result29:");
+prntArray(arr);
